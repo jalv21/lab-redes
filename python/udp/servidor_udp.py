ddcaa434880a846be1371cd6b9d59ec743ae95e9
Jalv <araujo.ja.rodrigues@gmail.com>
2026-08-15 11:50:16 -0300

    feat(udp): testando cliente e servidor UDP em Python

--- python/udp/servidor_udp.py
+++ python/udp/servidor_udp.py
@@ -7,10 +7,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as servidor:
     servidor.bind((HOST, PORTA))
     print(f"[UDP] Servidor aguardando datagramas na porta {PORTA}...")
 
-    while True:
-        dados, endereco_cliente = servidor.recvfrom(1024)
-        mensagem = dados.decode("utf-8")
-        print(f"[UDP] Recebido de {endereco_cliente}: {mensagem}")
+    try:
+        while True:
+            dados, endereco_cliente = servidor.recvfrom(1024)
+            mensagem = dados.decode("utf-8")
+            print(f"[UDP] Recebido de {endereco_cliente}: {mensagem}")
 
-        resposta = f'Monitor responde: recebi sua mensagem -> "{mensagem}"'
-        servidor.sendto(resposta.encode("utf-8"), endereco_cliente)
+            resposta = f'Monitor responde: recebi sua mensagem -> "{mensagem}"'
+            servidor.sendto(resposta.encode("utf-8"), endereco_cliente)
+            
+    except KeyboardInterrupt:
+        print("\n[UDP] Encerrando servidor...")
