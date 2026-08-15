18c3f299d41ec15228e20ee07706e68301b99411
Jalv <araujo.ja.rodrigues@gmail.com>
2026-08-14 21:07:36 -0300

    criando cliente UDP Java

---
+++ java/udp/ClienteUDP.java
@@ -0,0 +1,33 @@
+import java.net.*;
+import java.util.Scanner;
+
+public class ClienteUDP {
+    public static void main(String[] args) throws Exception {
+        String host = "localhost";
+        int porta = 5042;
+
+        Scanner teclado = new Scanner(System.in);
+
+        try(DatagramSocket socket = new DatagramSocket()) {
+            InetAddress endereco = InetAddress.getByName(host);
+            byte[] buffer = new byte[1024];
+
+            System.out.println("[UDP] Pronto para enviar. Digite 'sair' para encerrar.");
+            while(true) {
+                System.out.print("> ");
+                String mensagem = teclado.nextLine();
+                byte[] dados = mensagem.getBytes();
+
+                DatagramPacket pacote = new DatagramPacket(dados, dados.length, endereco, porta);
+                socket.send(pacote);
+                if(mensagem.equalsIgnoreCase("sair")) break;
+
+                DatagramPacket resposta = new DatagramPacket(buffer, buffer.length);
+                socket.receive(resposta);
+                System.out.println(new String(resposta.getData(), 0, resposta.getLength()));
+            }
+        }
+
+        teclado.close();
+    }
+}
