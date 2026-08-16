7a30b4b48e6643ec16c879c8996dc75b6d1b252f
Jalv <araujo.ja.rodrigues@gmail.com>
2026-08-16 12:24:14 -0300

    feat(multicast): testando cliente e servidor multicast em ambas as linguagens

--- java/multicast/ClienteMulticast.java
+++ java/multicast/ClienteMulticast.java
@@ -13,8 +13,8 @@ public class ClienteMulticast {
             InetSocketAddress endpointGrupo = new InetSocketAddress(grupo, porta);
             
             // Em Wi-Fi corporativa/VPN o multicast costuma ser bloqueado. Para testar servidor e cliente na MESMA máquina, use a interface de loopback explicitamente:
-            NetworkInterface interfaceRede = NetworkInterface.getByInetAddress(InetAddress.getLoopbackAddress());
-            // NetworkInterface interfaceRede = NetworkInterface.getByInetAddress(InetAddress.getLocalHost());
+            // NetworkInterface interfaceRede = NetworkInterface.getByInetAddress(InetAddress.getLoopbackAddress());
+            NetworkInterface interfaceRede = NetworkInterface.getByInetAddress(InetAddress.getLocalHost());
 
             socket.joinGroup(endpointGrupo, interfaceRede);
             System.out.println("[Multicast] Inscrito no grupo " + grupoMulticast + ": " + porta + ". Aguardando avisos...");
